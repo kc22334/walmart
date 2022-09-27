8aee1f77ab72fc28591819b6a62a86ffdc3a28ee
kumar22334 <kumarhem22334@gmail.com>
2022-09-27 10:25:55 +0530

    del

--- deleterepo.sh
+++ deleterepo.sh
@@ -1,7 +1,6 @@
 echo "deleting the repo..."
 curl \
         -X DELETE \
-        -H Authorization: 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 \
         -H "Accept: application/vnd.github.v3+json" \
         https://api.github.com/repos/kc22334/wallmart-apirepo \
 	echo "deleted"
