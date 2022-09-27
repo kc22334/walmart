bbb095913e1314bdc3adb4fb1dd71d584bc27b2b
kumar22334 <kumarhem22334@gmail.com>
2022-09-27 10:11:56 +0530

    del repo

--- createrepo.sh
+++ createrepo.sh
@@ -1,6 +1,6 @@
 echo "creating the repo..."
 curl \
-	-X POST \
+	-X DELETE \
 	-u kumar22334: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 \
 	-H "Accept: application/vnd.github.v3+json" \
 	https://api.github.com/orgs/kc22334/repos \
